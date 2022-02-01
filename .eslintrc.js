5231fbb47bc61e6fbc42c05725a21df2de66e001
Yong Yan <yanyong-gh@outlook.com>
2022-01-31 16:37:44 -0800

    Add files via upload

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,11 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
+    "extends": "eslint:recommended",
     "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
-    "plugins": [
-        "react"
-    ],
     "rules": {
     }
 }
