820d1e6867cec9a97780140e20663891f44c9702
Yong Yan <yanyong-gh@outlook.com>
2022-03-02 17:49:00 -0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "commonjs": true,
         "es2021": true,
-        "jest": true
+        "mocha": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
